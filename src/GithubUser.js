6f94a34f36e06ddf707b55172e3401ca6dcfb63a
Jasmine <singh486@purdue.edu>
2018-05-29 15:36:17 -0400

    Add Github user page

---
+++ src/GithubUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GithubUser extends Component{
+    render(){
+        return(
+            <div className="GithubUser">
+                <h1>Github user</h1>
+            </div>
+        )
+    }
+}
+
+export default GithubUser

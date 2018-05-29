33089eb81eb7d04685de41926250657578fe64a4
Jasmine <singh486@purdue.edu>
2018-05-29 15:44:22 -0400

    Make fetch happen

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
 
 class GithubUser extends Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            user: {}
+        }
+
+        this.fetchUserData()
+    }
+
+    fetchUserData = ()=>{
+        fetch(`https://api.github.com/users/${this.props.match.params.username}`)
+            .then(response => response.json())
+            .then(user => this.setState({user}))
+    }
     render(){
         return(
             <div className="GithubUser">
-                <h1>Github user</h1>
+                <h1>Github user: {this.props.match.params.username}</h1>
             </div>
         )
     }
